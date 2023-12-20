853acf34b060f82b7df407cf2a2dfcd19f3bbf01
sezim-programist <153936312+sezim-programist@users.noreply.github.com>
2023-12-20 10:58:45 +0300

    Update App.js

--- js-react/src/App.js
+++ js-react/src/App.js
@@ -1,77 +1,41 @@
 import React, { useState } from 'react';
-import './index.css';
 
-function App() {
-  const [colorSquare1, setColorSquare1] = useState('red');
-  const [colorSquare2, setColorSquare2] = useState('blue');
+function Todo() {
 
-  const handleSquareClick = () => {
-    // Меняем цвета квадратов местами
-    const tempColor = colorSquare1;
-    setColorSquare1(colorSquare2);
-    setColorSquare2(tempColor);
+  const [tasks, setTasks] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+ 
+  const addTask = () => {
+    if (inputText.trim() !== '') {
+      setTasks([...tasks, { text: inputText, completed: false }]);
+      setInputText(''); 
+    }
   };
 
   return (
-    <div className="App">
-      <div
-        className="square"
-        style={{ backgroundColor: colorSquare1 }}
-        onClick={handleSquareClick}
-      ></div>
-      <div
-        className="square"
-        style={{ backgroundColor: colorSquare2 }}
-        onClick={handleSquareClick}
-      ></div>
+    <div>
+      <h1>Todo </h1>
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="Добавить задачу"
+        />
+        <button onClick={addTask}>Добавить</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span>
+            {task.text}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
-
-
-
-/*import { useState } from "react";
-
-export default function MyApp(){
-  return (
-    <div>
-      <h1>Counters that update suparaterely</h1>
-      <MyButton />
-
-      <MyButton />
-    </div>
-  )
-}
-function MyButton() {
-  const[count, setCount] = useState(0)
-
-  function handleClick(){
-    setCount(count +1)
-  }
-  
-  return<button onClick={handleClick}>Clicked {count} times</button>
-}
-
-export default function Search(){
-  function publish(formData) {
-  const content = formData.get ("content");
-  const button = formData.get ("button");
-
-  alert(`'${content} bsvkvivugu "$(button it)'`);
-  }
-  function save(formData) {
-    const content = formData. get ("content");
-    alert(`your draft of '${content}' has been saved!`)
-  }
-return(
-  <form action="{publish}">
-    <textarea name="content" rows={4} cols={40}/>
-    <br/>
-    <button type="sub"
-
-  </form>
-)
-}*/
-
+export default Todo;
